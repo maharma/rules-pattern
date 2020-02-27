e5a50b002de8e2aacedd92b58dba7de8ddffcdb5
Prashant Kurlekar <prashantkurlekar@gmail.com>
2020-02-27 19:38:39 +1100

    Hospital Rule

---
+++ RulesPattern.Tests/Rules/HospitalRuleTests.cs
@@ -0,0 +1,46 @@
+﻿using RulesPattern.Models;
+using RulesPattern.Models.Enums;
+using RulesPattern.Rules;
+using System.Linq;
+using Xunit;
+
+namespace RulesPattern.Tests.Rules
+{
+    public class HospitalRuleTests
+    {
+        private readonly IRecommendationRule _hospitalRule;
+
+        public HospitalRuleTests()
+        {
+            _hospitalRule = new HospitalRule();
+        }
+
+        [Fact]
+        public void FilterProducts_Returns_ProductsForExtrasTierEssential()
+        {
+            // Arrange
+            var choices = new Choices { Hospital = Hospital.Public };
+
+            // Act
+            var productsQuery = _hospitalRule.Evaluate(choices, MockData.Products);
+            var products = productsQuery.ToList();
+
+            // Assert
+            Assert.Equal(3, products.Count);
+        }
+
+        [Fact]
+        public void FilterProducts_Returns_ProductsForExtrasTierIntermediate()
+        {
+            // Arrange
+            var choices = new Choices { Hospital = Hospital.PublicPrivate };
+
+            // Act
+            var productsQuery = _hospitalRule.Evaluate(choices, MockData.Products);
+            var products = productsQuery.ToList();
+
+            // Assert
+            Assert.Equal(6, products.Count);
+        }
+    }
+}
--- RulesPattern/Rules/HospitalRule.cs
+++ RulesPattern/Rules/HospitalRule.cs
@@ -1,6 +1,14 @@
-﻿namespace RulesPattern.Rules
+﻿using System.Collections.Generic;
+using RulesPattern.Models;
+using System.Linq;
+
+namespace RulesPattern.Rules
 {
-    public class HospitalRule
+    public class HospitalRule : IRecommendationRule
     {
+        public IQueryable<Product> Evaluate(Choices choices, IEnumerable<Product> products)
+        {
+            return products.Where(p => p.Hospital == choices.Hospital).AsQueryable();
+        }
     }
 }
