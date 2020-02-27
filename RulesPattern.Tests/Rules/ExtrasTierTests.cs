d778aa7b03bfb1924e7dc1a22fdc9e9a054a4c21
Prashant Kurlekar <prashantkurlekar@gmail.com>
2020-02-27 19:27:39 +1100

    Extras Tier Rule

---
+++ RulesPattern.Tests/Rules/ExtrasTierTests.cs
@@ -0,0 +1,60 @@
+﻿using RulesPattern.Models;
+using RulesPattern.Models.Enums;
+using RulesPattern.Rules;
+using System.Linq;
+using Xunit;
+
+namespace RulesPattern.Tests.Rules
+{
+    public class ExtrasTierTests
+    {
+        private readonly IRecommendationRule _extrasTierRule;
+
+        public ExtrasTierTests()
+        {
+            _extrasTierRule = new ExtrasTierRule();
+        }
+
+        [Fact]
+        public void FilterProducts_Returns_ProductsForExtrasTierEssential()
+        {
+            // Arrange
+            var choices = new Choices { ExtrasTier = ExtrasTier.Essential };
+
+            // Act
+            var productsQuery = _extrasTierRule.Evaluate(choices, MockData.Products);
+            var products = productsQuery.ToList();
+
+            // Assert
+            Assert.Equal(3, products.Count);
+        }
+
+        [Fact]
+        public void FilterProducts_Returns_ProductsForExtrasTierIntermediate()
+        {
+            // Arrange
+            var choices = new Choices { ExtrasTier = ExtrasTier.Intermediate };
+
+            // Act
+            var productsQuery = _extrasTierRule.Evaluate(choices, MockData.Products);
+            var products = productsQuery.ToList();
+
+            // Assert
+            Assert.Equal(3, products.Count);
+        }
+
+        [Fact]
+        public void FilterProducts_Returns_ProductsForExtrasTierTop()
+        {
+            // Arrange
+            var choices = new Choices { ExtrasTier = ExtrasTier.Top };
+
+            // Act
+            var productsQuery = _extrasTierRule.Evaluate(choices, MockData.Products);
+            var products = productsQuery.ToList();
+
+            // Assert
+            Assert.Equal(3, products.Count);
+        }
+    }
+}
--- RulesPattern/Rules/ExtrasTierRule.cs
+++ RulesPattern/Rules/ExtrasTierRule.cs
@@ -1,6 +1,14 @@
-﻿namespace RulesPattern.Rules
+﻿using System.Collections.Generic;
+using RulesPattern.Models;
+using System.Linq;
+
+namespace RulesPattern.Rules
 {
-    public class ExtrasTierRule
+    public class ExtrasTierRule : IRecommendationRule
     {
+        public IQueryable<Product> Evaluate(Choices choices, IEnumerable<Product> products)
+        {
+            return products.Where(p => p.ExtrasTier == choices.ExtrasTier).AsQueryable();
+        }
     }
 }
